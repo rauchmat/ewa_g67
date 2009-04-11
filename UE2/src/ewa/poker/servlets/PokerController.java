529078db93a272f1a552c1eb8391d7a3ab59e643
Matthias Rauch <rauchmat@gmail.com>
2009-04-11 14:32:48 +0200

    added PokerController and table.jsp

---
+++ UE2/src/ewa/poker/servlets/PokerController.java
@@ -0,0 +1,38 @@
+package ewa.poker.servlets;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import sun.reflect.ReflectionFactory.GetReflectionFactoryAction;
+
+public class PokerController extends HttpServlet {
+
+	private static final String NEWGAME_KEY = "newGame";
+	private static final String CHANGEUSERDATA_KEY = "changeUserData";
+	private static final String LOGOUT_KEY = "logout";
+	
+	private static final long serialVersionUID = 7830604605299588034L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		if (req.getParameterMap().containsKey(NEWGAME_KEY)) {
+			RequestDispatcher disp = getServletContext().getRequestDispatcher("/table.jsp");
+			disp.forward(req, resp);
+		}
+		if (req.getParameterMap().containsKey(CHANGEUSERDATA_KEY)) {
+			//TODO: handle request
+		}
+		if (req.getParameterMap().containsKey(LOGOUT_KEY)) {
+			//TODO: handle request
+		}
+	}
+}
