5d4d97c5846167e2afd447a58a3b12f3653c54c0
Patrick Marschik <patrick.marschik@gmail.com>
2009-06-07 12:28:07 +0200

    Fixed make_ue3.sh Removed Build files Modified .gitignore

--- abgabe/make_ue3.sh
+++ abgabe/make_ue3.sh
@@ -13,20 +13,20 @@ BSP="3"
 FILENAME="UE${BSP}-AG_${GRP}_gruppe-${MNR1}-${MNR2}"
 
 echo "Making directories ..."
-mkdir ./UE3 &> /dev/null
-mkdir ./UE3/src &> /dev/null
-mkdir ./UE3/WebContent &> /dev/null
+mkdir ./UE${BSP} &> /dev/null
+mkdir ./UE${BSP}/src &> /dev/null
+mkdir ./UE${BSP}/WebContent &> /dev/null
 
 echo "Cleaning directories ..."
-rm -Rf ./UE3/src/*
-rm -Rf ./UE3/WebContent/*
+rm -Rf ./UE${BSP}/src/*
+rm -Rf ./UE${BSP}/WebContent/*
 
 echo "Copying files ..."
-cp -Rf ../UE3/src/* ./UE2/src/ > /dev/null
-cp -Rf ../UE3/WebContent/* ./UE2/WebContent/ > /dev/null
+cp -Rf ../UE${BSP}/src/* ./UE${BSP}/src/ > /dev/null
+cp -Rf ../UE${BSP}/WebContent/* ./UE${BSP}/WebContent/ > /dev/null
 
 echo "Creating ZIP ..."
-zip -r -9 "${FILENAME}.zip" UE2/* -x "*.DS_Store" > /dev/null
+zip -r -9 "${FILENAME}.zip" UE${BSP}/* -x "*.DS_Store" > /dev/null
 
 echo
 echo "Finished creating ${FILENAME}.zip"
